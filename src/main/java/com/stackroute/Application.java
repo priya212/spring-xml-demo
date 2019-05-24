ad1c4372baab5be302c0b14ac43d9dd562433ddc
priyanka <darekarpriyanka2@gmail.com>
2019-05-24 13:43:33 +0530

    Task 2 completed

--- src/main/java/com/stackroute/Application.java
+++ src/main/java/com/stackroute/Application.java
@@ -1,6 +1,7 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import  org.springframework.core.io.Resource;
 import org.springframework.beans.factory.BeanFactory;
@@ -17,6 +18,8 @@ public class Application {
         Movie movie1=(Movie)context.getBean("movie");
         System.out.println(movie1.getId()+" "+movie1.getActor()+ " acted in : "+movie1.getMovieName());
 
+
+
         Movie movie2=(Movie)context.getBean("movie1");
         System.out.println(movie2.getId()+" "+movie2.getActor()+ " acted in : "+movie2.getMovieName());
 
@@ -30,6 +33,14 @@ public class Application {
         ApplicationContext context1=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie4=(Movie)context.getBean("movie2");
         System.out.println(movie3==movie4);
+
+        //Movie bean by its two different name
+        Movie movie6=(Movie)context.getBean("movieA");
+        Movie movie7=(Movie)context.getBean("movieB");
+        System.out.println(movie6.getId()+" "+movie6.getActor()+ " acted in : "+movie6.getMovieName());
+        System.out.println(movie7.getId()+" "+movie7.getActor()+ " acted in : "+movie7.getMovieName());
+
+
 
 
     }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -11,6 +11,10 @@ public class Movie {
         this.actor = actor;
     }
 
+    public Movie(int id, String movieName) {
+        this.id = id;
+        this.movieName = movieName;
+    }
 
     public Movie(int id, String movieName, Actor actor) {
         this.id = id;
