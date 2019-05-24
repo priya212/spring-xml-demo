3789d8bbc22f0abeceda5d4d4c0560c61623ca51
priyanka <darekarpriyanka2@gmail.com>
2019-05-24 14:44:13 +0530

    Task 3 completed

--- src/main/java/com/stackroute/Application.java
+++ src/main/java/com/stackroute/Application.java
@@ -12,36 +12,14 @@ import org.springframework.core.io.ClassPathResource;
 
 public class Application {
     public static void main( String[] args ) {
-        System.out.println("Using Application Context");
-
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("Using autowire byName");
         Movie movie1=(Movie)context.getBean("movie");
         System.out.println(movie1.getId()+" "+movie1.getActor()+ " acted in : "+movie1.getMovieName());
 
-
-
+        /*System.out.println("Using autowire byType");
         Movie movie2=(Movie)context.getBean("movie1");
-        System.out.println(movie2.getId()+" "+movie2.getActor()+ " acted in : "+movie2.getMovieName());
-
-        Movie movie3=(Movie)context.getBean("movie2");
-        System.out.println(movie3.getId()+" "+movie3.getActor()+ " acted in : "+movie3.getMovieName());
-
-        Movie movie5=(Movie)context.getBean("movie1");
-        System.out.println(movie2==movie5);
-
-        // testing bean scope
-        ApplicationContext context1=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie4=(Movie)context.getBean("movie2");
-        System.out.println(movie3==movie4);
-
-        //Movie bean by its two different name
-        Movie movie6=(Movie)context.getBean("movieA");
-        Movie movie7=(Movie)context.getBean("movieB");
-        System.out.println(movie6.getId()+" "+movie6.getActor()+ " acted in : "+movie6.getMovieName());
-        System.out.println(movie7.getId()+" "+movie7.getActor()+ " acted in : "+movie7.getMovieName());
-
-
-
+        System.out.println(movie2.getId()+" "+movie2.getActor()+ " acted in : "+movie2.getMovieName());*/
 
     }
 }
